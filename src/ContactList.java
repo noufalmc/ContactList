bd709b03a61de19823aac573364d2e49c80fc11f
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2022-01-02 08:49:33 +0530

    Added:Create Contacts

--- src/ContactList.java
+++ src/ContactList.java
@@ -7,6 +7,28 @@ Create Address Book Using ArrayList
 * */
 package corejava;
 
+import java.util.Scanner;
+
+class  Contacts
+{
+    String firstName;
+    String lastName;
+    String city;
+    String address;
+    String email;
+    long mobile;
+    int pincode;
+    Contacts(String firstName,String lastName,String city,String address,String email,long mobile,int pincode)
+    {
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.city=city;
+        this.address=address;
+        this.email=email;
+        this.mobile=mobile;
+        this.pincode=pincode;
+    }
+}
 public class ContactList {
 
     public static void main(String[] args) {
