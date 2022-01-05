b94d528848fc9eb7b2679fba16b97117e964c354
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2022-01-05 16:14:10 +0530

    Added:Class Seperate file added

--- src/corejava/ContactList.java
+++ src/corejava/ContactList.java
@@ -9,54 +9,6 @@ package corejava;
 
 import java.util.ArrayList;
 import java.util.*;
-class  Contacts
-{
-    String firstName;
-    String lastName;
-    String city;
-    String address;
-    String email;
-    String mobile;
-    int pincode;
-    Contacts(String firstName,String lastName,String city,String address,String email,String mobile,int pincode)
-    {
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.city=city;
-        this.address=address;
-        this.email=email;
-        this.mobile=mobile;
-        this.pincode=pincode;
-    }
-    public void setFirstName(String  firstName)
-    {
-        this.firstName=firstName;
-    }
-    public void setLastName(String  lastName)
-    {
-        this.lastName=lastName;
-    }
-    public void setAddress(String address)
-    {
-        this.address=address;
-    }
-    public void setCity(String city)
-    {
-        this.city=city;
-    }
-    public void setEmail(String email)
-    {
-        this.email=email;
-    }
-    public  void  setMobile(String mobile)
-    {
-        this.mobile=mobile;
-    }
-    public void  setPincode(int pincode)
-    {
-        this.pincode=pincode;
-    }
-}
 class Address
 {
     ArrayList<Contacts> list=new ArrayList<>();
